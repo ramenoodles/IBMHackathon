6e63b8fb705138477a25541f5645b67a4edc4a8c
ramenoodles <ramksenthil@gmail.com>
2026-08-28 16:27:44 -0400

    Some visual changes for the website

--- OnBober/src/components/HelloWorld.vue
+++ OnBober/src/components/HelloWorld.vue
@@ -8,9 +8,7 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      The IBM Bob powered onboarding tool!
     </h3>
   </div>
 </template>
--- OnBober/src/views/AboutView.vue
+++ OnBober/src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h3>Joining a new company or team can be a daunting task, especially when trying to understand a new codebase. 
+      Team specific practices, functions, and documentation all need to be interpreted and understood quickly.
+
+    </h3>
   </div>
 </template>
 
